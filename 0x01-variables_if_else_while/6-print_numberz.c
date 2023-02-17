5c05d6c7dd04f817bcbf779b58883be0f78bbc08
nathanbarreh <nathanbarreh4@gmail.com>
2023-02-17 10:30:08 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,18 @@
  * You can only use the putchar function
  * Return: 0
  */
-
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-printf("\n");
-return (0);
+	putchar(48);
+	putchar(49);
+	putchar(50);
+	putchar(51);
+	putchar(52);
+	putchar(53);
+	putchar(54);
+	putchar(55);
+	putchar(56);
+	putchar(57);
+	putchar('\n');
+	return (0);
 }
