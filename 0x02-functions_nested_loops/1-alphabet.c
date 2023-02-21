a7c3af9520a2ea1abdbf2271bc937bcd20f83e38
nathanbarreh <nathanbarreh4@gmail.com>
2023-02-21 07:48:01 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet, lowercase
+ * 
+ * Return: nada.
+ */
+
+void print_alphabet(void)
+{
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l += 1;
+	}
+	_putchar(10);
+}
