347bb8bbb998058eb3e021d4d9d0181dc82e0bd2
nathanbarreh <nathanbarreh4@gmail.com>
2023-02-21 06:38:26 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints "_putchar" followed by a new line
+ * and returns 0 to the operating system.
+ *
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
