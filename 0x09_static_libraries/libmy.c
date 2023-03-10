860fd9e13e0edfa6995f95e10056bfe198462064
nathanbarreh <nathanbarreh4@gmail.com>
2023-03-09 23:59:33 -0800

    0x09_static_libraries

---
+++ 0x09_static_libraries/libmy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main- library that contains functions
+* main_function: function to be executed
+* main.h: static library
+* return: nothing
+*/
+
+
+int main()
+{
+main();
+return (0);
+}
