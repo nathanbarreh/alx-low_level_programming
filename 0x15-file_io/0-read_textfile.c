fec5db95ced16d8ea10bffb40ffa584fc9cccf84
nathanbarreh <nathanbarreh4@gmail.com>
2023-04-13 06:27:26 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,26 @@
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <main.h>
-
+#include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to the POSIX stdout
- *
- * @filename: Path to file to read and print
- * @letters: Maximum number of letters to read and print
- *
- * Return: Number of letters read and printed, or 0 on failure
+ * read_textfile - singly linked list
+ * @filename: string - (malloc'ed string)
+ * @letters: its a letter
+ * Return: a number
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int input_fd;
-	ssize_t lenr, lenw;
-	char *buffer;
+	ssize_t fd, write_data = 0, read_data = 0;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-
-	input_fd = open(filename, O_RDONLY);
-	if (input_fd == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-	}
-
-	lenr = read(input_fd, buffer, letters);
-	close(input_fd);
-
-	if (lenr == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-
-	if (lenr != lenw)
-		return (0);
-
-	return (lenw);
+	read_data = read(fd, buf, letters);
+	write_data = write(STDOUT_FILENO, buf, read_data);
+	close(fd);
+	free(buf);
+	return (write_data);
 }
-
